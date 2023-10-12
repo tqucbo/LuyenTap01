49cfe6d0127cbca9fbe37687cfbe1e193ef28401
DESKTOP-SU17G1L\TUF <t.qucbo@gmail.com>
2023-10-12 11:27:30 +0700

    Vận dụng delegate Action, delegate Func và biểu thức Lambda trong việc tính tổng, hiệu và tích vô hướng của 2 Vector.

--- LuyenTap01/Program.cs
+++ LuyenTap01/Program.cs
@@ -7,10 +7,17 @@ class Program
     {
         Vector v1 = new Vector();
         v1.nhapVector();
-        Console.WriteLine(v1.xuatVector());
+        // Console.WriteLine(v1.xuatVector());
+        Action<string> action = Vector.xuatThongTin;
+        action.Invoke($"Vecto 1 co toa do la : ({v1.a}, {v1.b})");
         Vector v2 = new Vector();
         v2.nhapVector();
-        Console.WriteLine(v2.xuatVector());
+        // Console.WriteLine(v2.xuatVector());
+        action.Invoke($"Vecto 2 co toa do la : ({v2.a}, {v2.b})");
+        Console.WriteLine("Nhap so do goc giua 2 Vecto : ");
+        int gocGiua2Vector = int.Parse(Console.ReadLine());
+        action.Invoke($"Goc giua 2 Vecto la {gocGiua2Vector} do.");
+
 
     // Vector v3 = Vector.tinhTong2Vector(v1, v2);
     // Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
@@ -23,38 +30,45 @@ class Program
         Console.WriteLine("1. Tinh tong 2 Vector vua nhap");
         Console.WriteLine("2. Tinh hieu 2 Vector vua nhap");
         Console.WriteLine("3. Tinh tich vo huong 2 Vector vua nhap");
+        Console.WriteLine("all. Thuc hien ca 3 phuong thuc tren");
+        Console.WriteLine("exit. Thoat");
         string switchNumber = Console.ReadLine();
         switch (switchNumber)
         {
             case "1":
-                Vector v3 = Vector.tinhTong2Vector(v1, v2);
-                Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
-                // break;
+                // Vector v3 = Vector.tinhTong2Vector(v1, v2);
+                // Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
+                // // break;
+                Func<Vector, Vector, Vector> tinhTong = Vector.tinhTong2Vector;
+                Console.WriteLine($"Tong cua 2 Vector vua nhap la mot Vector co toa do la : ({tinhTong(v1, v2).a}, {tinhTong(v1, v2).b})");
                 goto L1;
             case "2":
-                Vector v4 = Vector.tinhHieu2Vector(v1, v2);
-                Console.WriteLine($"Tong 2 Vector vua nhap : " + v4.xuatVector());
-                // break;
+                // Vector v4 = Vector.tinhHieu2Vector(v1, v2);
+                // Console.WriteLine($"Tong 2 Vector vua nhap : " + v4.xuatVector());
+                // // break;
+                Func<Vector, Vector, Vector> tinhHieu = Vector.tinhHieu2Vector;
+                Console.WriteLine($"Hieu cua 2 Vector vua nhap la mot Vector co toa do la : ({tinhHieu(v1, v2).a}, {tinhHieu(v1, v2).b})");
                 goto L1;
             case "3":
-                double tichVoHuong = Vector.tinhTichVoHuong2Vector(v1, v2);
-                Console.WriteLine($"Tich vo huong 2 Vector vua nhap : {tichVoHuong}");
+
+                Vector.tinhTichVoHuong2Vector(v1, v2, gocGiua2Vector, Vector.xuatThongTin);
+                // double tichVoHuong = Vector.tinhTichVoHuong2Vector(v1, v2);
+                // Console.WriteLine($"Tich vo huong 2 Vector vua nhap : {tichVoHuong}");
                 // break;
                 goto L1;
             case "all":
             case "a":
-                v3 = Vector.tinhTong2Vector(v1, v2);
-                Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
-                v4 = Vector.tinhHieu2Vector(v1, v2);
-                Console.WriteLine($"Tong 2 Vector vua nhap : " + v4.xuatVector());
-                tichVoHuong = Vector.tinhTichVoHuong2Vector(v1, v2);
-                Console.WriteLine($"Tich vo huong 2 Vector vua nhap : {tichVoHuong}");
+                tinhTong = Vector.tinhTong2Vector;
+                Console.WriteLine($"Tong cua 2 Vector vua nhap la mot Vector co toa do la : ({tinhTong(v1, v2).a}, {tinhTong(v1, v2).b})");
+                tinhHieu = Vector.tinhHieu2Vector;
+                Console.WriteLine($"Hieu cua 2 Vector vua nhap la mot Vector co toa do la : ({tinhHieu(v1, v2).a}, {tinhHieu(v1, v2).b})");
+                Vector.tinhTichVoHuong2Vector(v1, v2, gocGiua2Vector, Vector.xuatThongTin);
                 // break;
                 goto L1;
             case "exit":
                 break;
             default:
-                Console.WriteLine("Khong co lua chon nay.");
+                action.Invoke("Khong co thong tin nay");
                 goto L1;
         }
     }
--- LuyenTap01/Vector.cs
+++ LuyenTap01/Vector.cs
@@ -1,9 +1,12 @@
+using System.Runtime.Intrinsics;
+
 namespace NameSpaceVector
 {
+    public delegate void hienThiThongBao(string thongBao);
     class Vector
     {
-        double a;
-        double b;
+        public double a;
+        public double b;
         public Vector(double a, double b)
         {
             this.a = a;
@@ -23,35 +26,48 @@ namespace NameSpaceVector
             b = double.Parse(Console.ReadLine());
         }
 
-        public string xuatVector()
+        // public string xuatVector()
+        // {
+        //     return $"Vecto co toa do ({a}, {b})";
+        // }
+        public static void xuatThongTin(string s) => Console.WriteLine(s);
+        // public static Vector tinhTong2Vector(Vector v1, Vector v2)
+        // {
+        //     Vector v3 = new Vector();
+        //     v3.a = v1.a + v2.a;
+        //     v3.b = v1.b + v2.b;
+        //     return v3;
+        // }
+        public static Vector tinhTong2Vector(Vector v1, Vector v2) => new Vector(v1.a + v2.a, v1.b + v2.b);
+        // public static Vector tinhHieu2Vector(Vector v1, Vector v2)
+        // {
+        //     Vector v3 = new Vector();
+        //     v3.a = v1.a - v2.a;
+        //     v3.b = v1.b - v2.b;
+        //     return v3;
+        // }
+        public static Vector tinhHieu2Vector(Vector v1, Vector v2) => new Vector(v1.a - v2.a, v1.b - v2.b);
+
+        // public static double tinhTichVoHuong2Vector(Vector v1, Vector v2)
+        // {
+        //     double kq;
+        //     double doDaiV1 = Math.Sqrt(v1.a * v1.a + v1.b * v1.b);
+        //     Console.WriteLine(doDaiV1);
+        //     double doDaiV2 = Math.Sqrt(v2.a * v2.a + v2.b * v2.b);
+        //     Console.WriteLine(doDaiV2);
+        //     double cosV1V2 = (v1.a * v2.a + v1.b * v2.b) / (Math.Sqrt(v1.a * v1.a + v1.b * v2.b) + Math.Sqrt(v2.a * v2.a + v2.b * v2.b));
+        //     Console.WriteLine(cosV1V2);
+        //     kq = doDaiV1 * doDaiV2 * cosV1V2;
+        //     return kq;
+        // }
+
+        public static void tinhTichVoHuong2Vector(Vector v1, Vector v2, int gocGiua2Vector, hienThiThongBao thongBao)
         {
-            return $"Vecto co toa do ({a}, {b})";
-        }
-        public static Vector tinhTong2Vector(Vector v1, Vector v2)
-        {
-            Vector v3 = new Vector();
-            v3.a = v1.a + v2.a;
-            v3.b = v1.b + v2.b;
-            return v3;
-        }
-        public static Vector tinhHieu2Vector(Vector v1, Vector v2)
-        {
-            Vector v3 = new Vector();
-            v3.a = v1.a - v2.a;
-            v3.b = v1.b - v2.b;
-            return v3;
-        }
-        public static double tinhTichVoHuong2Vector(Vector v1, Vector v2)
-        {
-            double kq;
             double doDaiV1 = Math.Sqrt(v1.a * v1.a + v1.b * v1.b);
-            Console.WriteLine(doDaiV1);
             double doDaiV2 = Math.Sqrt(v2.a * v2.a + v2.b * v2.b);
-            Console.WriteLine(doDaiV2);
-            double cosV1V2 = (v1.a * v2.a + v1.b * v2.b) / (Math.Sqrt(v1.a * v1.a + v1.b * v2.b) + Math.Sqrt(v2.a * v2.a + v2.b * v2.b));
-            Console.WriteLine(cosV1V2);
-            kq = doDaiV1 * doDaiV2 * cosV1V2;
-            return kq;
+            double cosV1V2 = Math.Cos(gocGiua2Vector / 180 * Math.PI);
+            // Console.WriteLine(1 / 2 * Math.PI);
+            thongBao.Invoke($"Tich vo huong cua 2 Vecto vua nhap co gia tri la {doDaiV1 * doDaiV2 * cosV1V2}");
         }
     }
 }
