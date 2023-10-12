fae998d5fc0e90f7dbde21394c70d24b69064e54
DESKTOP-SU17G1L\TUF <t.qucbo@gmail.com>
2023-10-12 09:12:16 +0700

    - Thay vì xuất thẳng 3 phuong thức, dùng Switch - Case để cho người dùng chọn 1 trong 3 phương thức.

--- LuyenTap01/Program.cs
+++ LuyenTap01/Program.cs
@@ -11,11 +11,36 @@ class Program
         Vector v2 = new Vector();
         v2.nhapVector();
         Console.WriteLine(v2.xuatVector());
-        Vector v3 = Vector.tinhTong2Vector(v1, v2);
-        Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
-        Vector v4 = Vector.tinhHieu2Vector(v1, v2);
-        Console.WriteLine($"Tong 2 Vector vua nhap : " + v4.xuatVector());
-        double tichVoHuong = Vector.tinhTichVoHuong2Vector(v1, v2);
-        Console.WriteLine($"Tich vo huong 2 Vector vua nhap : {tichVoHuong}");
+
+    // Vector v3 = Vector.tinhTong2Vector(v1, v2);
+    // Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
+    // Vector v4 = Vector.tinhHieu2Vector(v1, v2);
+    // Console.WriteLine($"Tong 2 Vector vua nhap : " + v4.xuatVector());
+    // double tichVoHuong = Vector.tinhTichVoHuong2Vector(v1, v2);
+    // Console.WriteLine($"Tich vo huong 2 Vector vua nhap : {tichVoHuong}");
+
+    L1: Console.WriteLine("Moi nhap lua chon : ");
+        Console.WriteLine("1. Tinh tong 2 Vector vua nhap");
+        Console.WriteLine("2. Tinh hieu 2 Vector vua nhap");
+        Console.WriteLine("3. Tinh tich vo huong 2 Vector vua nhap");
+        string switchNumber = Console.ReadLine();
+        switch (switchNumber)
+        {
+            case "1":
+                Vector v3 = Vector.tinhTong2Vector(v1, v2);
+                Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
+                break;
+            case "2":
+                Vector v4 = Vector.tinhHieu2Vector(v1, v2);
+                Console.WriteLine($"Tong 2 Vector vua nhap : " + v4.xuatVector());
+                break;
+            case "3":
+                double tichVoHuong = Vector.tinhTichVoHuong2Vector(v1, v2);
+                Console.WriteLine($"Tich vo huong 2 Vector vua nhap : {tichVoHuong}");
+                break;
+            default:
+                Console.WriteLine("Khong co lua chon nay.");
+                break;
+        }
     }
 }
