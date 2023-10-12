d339c5860d3594ba56f22541bac57f87ff69d7e2
DESKTOP-SU17G1L\TUF <t.qucbo@gmail.com>
2023-10-12 08:03:20 +0700

    1. Dinh nghia doi tuong Vector

---
+++ LuyenTap01/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace LuyenTap01;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ LuyenTap01/Vector.cs
@@ -0,0 +1,18 @@
+namespace Vector
+{
+    class Vector
+    {
+        double a;
+        double b;
+        public Vector(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Vector()
+        {
+            this.a = 0;
+            this.b = 0;
+        }
+    }
+}
