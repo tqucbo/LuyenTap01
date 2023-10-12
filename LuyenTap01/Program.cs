7f167b8810291fdf8bb55bf7f20a5e2ed49ca24a
DESKTOP-SU17G1L\TUF <t.qucbo@gmail.com>
2023-10-12 08:57:44 +0700

    - Nhập thông tin 2 Vector và xuất 2 Vector ra màn hình - Tạo phuong thức tính tổng, hiệu và tích vô hướng 2 Vector này - Xuất kết quả các phương thức này ra màn hình.

--- LuyenTap01/Program.cs
+++ LuyenTap01/Program.cs
@@ -1,8 +1,21 @@
-﻿namespace LuyenTap01;
+﻿using NameSpaceVector;
+
+namespace LuyenTap01;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Vector v1 = new Vector();
+        v1.nhapVector();
+        v1.xuatVector();
+        Vector v2 = new Vector();
+        v2.nhapVector();
+        v2.xuatVector();
+        Vector v3 = Vector.tinhTong2Vector(v1, v2);
+        Console.WriteLine($"Tong 2 Vector vua nhap : " + v3.xuatVector());
+        Vector v4 = Vector.tinhHieu2Vector(v1, v2);
+        Console.WriteLine($"Tong 2 Vector vua nhap : " + v4.xuatVector());
+        double tichVoHuong = Vector.tinhTichVoHuong2Vector(v1, v2);
+        Console.WriteLine($"Tich vo huong 2 Vector vua nhap : {tichVoHuong}");
     }
 }
--- LuyenTap01/Vector.cs
+++ LuyenTap01/Vector.cs
@@ -1,4 +1,4 @@
-namespace Vector
+namespace NameSpaceVector
 {
     class Vector
     {
@@ -14,5 +14,42 @@ namespace Vector
             this.a = 0;
             this.b = 0;
         }
+
+        public void nhapVector()
+        {
+            Console.WriteLine("Nhap toa do a : ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap toa do b : ");
+            b = double.Parse(Console.ReadLine());
+        }
+
+        public string xuatVector()
+        {
+            return $"Vecto co toa do ({a}, {b})";
+        }
+        public static Vector tinhTong2Vector(Vector v1, Vector v2)
+        {
+            Vector v3 = new Vector();
+            v3.a = v1.a + v2.a;
+            v3.b = v1.b + v2.b;
+            return v3;
+        }
+        public static Vector tinhHieu2Vector(Vector v1, Vector v2)
+        {
+            Vector v3 = new Vector();
+            v3.a = v1.a - v2.a;
+            v3.b = v1.b - v2.b;
+            return v3;
+        }
+        public static double tinhTichVoHuong2Vector(Vector v1, Vector v2)
+        {
+            double kq;
+            double doDaiV1 = Math.Sqrt(v1.a * v1.a + v1.b * v1.b);
+            double doDaiV2 = Math.Sqrt(v1.a * v1.a + v1.b * v1.b);
+            double cosV1V2 = (v1.a * v2.a + v1.b * v2.b) / (Math.Sqrt(v1.a * v1.a + v1.b * v2.b) + Math.Sqrt(v2.a * v2.a + v2.b * v2.b));
+            kq = doDaiV1 * doDaiV2 * cosV1V2;
+            return kq;
+        }
+
     }
 }
